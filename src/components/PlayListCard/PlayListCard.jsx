16ae5dc31fa9d0d146e85824fba578edd0f40a56
Andres Felipe Castro <pipecar366@gmail.com>
2024-01-27 08:02:56 -0500

    Se corrigieron link to en el platlistcard

--- src/components/PlayListCard/PlayListCard.jsx
+++ src/components/PlayListCard/PlayListCard.jsx
@@ -1,8 +1,9 @@
+import { Link } from "react-router-dom"
 import "./PlayListCard.css"
 
 const PlayListCard = ({playlist}) => {
     return (
-        <a href={`/playlist/${playlist.id}`} className="playlist_card">
+        <Link to={`/playlist/${playlist.id}`} className="playlist_card">
             <img src={playlist.cover} alt="" />
             <div className="info_playlist_card">
                 <h4>{playlist.title}</h4>
@@ -10,7 +11,7 @@ const PlayListCard = ({playlist}) => {
                     {playlist.artists.join(", ")}
                 </span>
             </div>
-        </a>
+        </Link>
     )
 }
 
